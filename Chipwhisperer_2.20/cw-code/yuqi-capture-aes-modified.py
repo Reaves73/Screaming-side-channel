94548514ccaf2aa9dd2e007c3d5f48b3ab8d914a
linux_in_mac <yqalll6@gmail.com>
2026-02-24 05:41:09 +0100

    do AES get traces and implement key recovery

---
+++ Chipwhisperer_2.20/cw-code/yuqi-capture-aes-modified.py
@@ -0,0 +1,110 @@
+import chipwhisperer as cw
+import cwhardware
+import time
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+PLATFORM = "CW308_STM32F0"
+#origin
+#fw_path = 'simpleserial-base-CW308_STM32F0.hex'
+
+#yuqi_try
+fw_path = 'simpleserial-aes-CW308_STM32F0.hex'
+
+
+#n_samples = 24000
+#post_len = int(n_samples - 11171/4)   # 只保留触发点后的 12000 点
+
+#yuqi_try
+n_samples = 12000
+
+n_traces = 500
+
+print("PLATFORM: ", PLATFORM)
+print("fw_path: ", fw_path)
+
+#
+# SETUP
+#
+
+hw = cwhardware.CWHardware()
+hw.connect(PLATFORM)
+
+# Confiture scope
+hw.scope.default_setup();
+hw.scope.adc.samples = n_samples
+
+#yuqi_try
+hw.scope.adc.decimate = 1
+
+#hw.scope.adc.decimate = 4
+hw.scope.clock.adc_src = "clkgen_x1"
+time.sleep(0.1)
+
+print("Target clock freq:", hw.scope.clock.clkgen_freq)
+print("Sampling rate:", hw.scope.clock.adc_rate)
+
+hw.program_target(fw_path)
+
+
+#
+# CAPTURE
+#
+
+ktp = cw.ktp.Basic()
+
+traces = np.zeros([n_traces, n_samples], dtype=np.float32)
+plaintexts = np.zeros([n_traces, 16], dtype=np.uint8)
+ciphertexts = np.zeros([n_traces, 16], dtype=np.uint8)
+keys = np.zeros([n_traces, 16], dtype=np.uint8)
+
+key, text = ktp.next()
+
+#target.set_key(key)
+
+print("Capturing traces...")
+
+
+for i in tqdm(range(n_traces)):
+    while True:
+        ret = hw.capture(text, key)
+
+        if ret is not None:
+            break
+    k = np.array(list(ret.key), dtype=np.uint8)
+    c = np.array(list(ret.textout), dtype=np.uint8)
+    p = np.array(list(ret.textin), dtype=np.uint8)
+    t = np.array(list(ret.wave), dtype=np.float32)
+
+    #t = np.array(ret.wave, dtype=np.float32)
+    #tc = hw.scope.adc.trig_count
+    #seg = t[int(tc/4): int(tc/4) + post_len]
+
+    traces[i] = t
+    #traces[i, :len(seg)] = seg
+    plaintexts[i] = p
+    ciphertexts[i] = c
+    keys[i] = k
+    
+    key, text = ktp.next() 
+
+
+#
+# DISCONNECT
+#
+
+hw.disconnect()
+
+np.save("data/traces.npy", traces)
+np.save("data/keys.npy", keys)
+np.save("data/plaintexts.npy", plaintexts)
+np.save("data/ciphertexts.npy", ciphertexts)
+
+plt.plot(np.average(traces, axis=0))
+#plt.plot(avg)
+#yuqi_try: draw line of trigger.
+#plt.axvline(x=0, color='red', linewidth=1)
+
+plt.show()
