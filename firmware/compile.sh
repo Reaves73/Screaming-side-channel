2b6df65e63cc1e633975450d4294b46abd822809
Andreas Lindner <andili@kth.se>
2026-04-17 14:25:55 +0200

    make runnable

--- firmware/compile.sh
+++ firmware/compile.sh
@@ -1,4 +1,7 @@
-%%bash
+#!/usr/bin/env bash
+
 #cd ../../firmware/mcu/simpleserial-base/
-cd ../../firmware/mcu/simpleserial-aes/
+#cd ../../firmware/mcu/simpleserial-aes/
+cd mcu/simpleserial-aes
+make PLATFORM=CW308_STM32F0 CRYPTO_TARGET=TINYAES128C clean
 make PLATFORM=CW308_STM32F0 CRYPTO_TARGET=TINYAES128C
