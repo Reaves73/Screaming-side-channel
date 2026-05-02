e712de2b40a02e430a2bb86d6dc095896c6afa1a
Andreas Lindner <andili@kth.se>
2026-05-02 11:40:01 +0200

    try with usrp -> seems unclean solution. requires lots of patches to work

--- software/collect_sharppeak/collect_sharppeak.py
+++ software/collect_sharppeak/collect_sharppeak.py
@@ -6,7 +6,7 @@
 #
 # GNU Radio Python Flow Graph
 # Title: collect_sharppeak
-# GNU Radio version: 3.10.9.2
+# GNU Radio version: 3.10.12.0
 
 from PyQt5 import Qt
 from gnuradio import qtgui
@@ -23,6 +23,9 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio import uhd
+import time
+import threading
 
 
 
@@ -49,7 +52,7 @@ class collect_sharppeak(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "collect_sharppeak")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "collect_sharppeak")
 
         try:
             geometry = self.settings.value("geometry")
@@ -57,13 +60,14 @@ class collect_sharppeak(gr.top_block, Qt.QWidget):
                 self.restoreGeometry(geometry)
         except BaseException as exc:
             print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+        self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
         ##################################################
         self.volume = volume = 0.500
         self.tx_gain = tx_gain = 10
-        self.samp_rate = samp_rate = 15e6
+        self.samp_rate = samp_rate = 5e6
         self.rf_gain = rf_gain = 10
         self.lsb_freq = lsb_freq = 434e6
         self.freq_shift = freq_shift = 12.5e6
@@ -72,40 +76,51 @@ class collect_sharppeak(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
 
-        self._volume_range = qtgui.Range(0, 100, 0.100, 0.500, 200)
-        self._volume_win = qtgui.RangeWidget(self._volume_range, self.set_volume, "Volume", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._volume_win)
-        self._tx_gain_range = qtgui.Range(0, 40, 1, 10, 200)
-        self._tx_gain_win = qtgui.RangeWidget(self._tx_gain_range, self.set_tx_gain, "TX Gain", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._tx_gain_win)
         self._rf_gain_range = qtgui.Range(0, 40, 1, 10, 200)
         self._rf_gain_win = qtgui.RangeWidget(self._rf_gain_range, self.set_rf_gain, "RX Gain", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._rf_gain_win)
         self._lsb_freq_range = qtgui.Range(425e6, 445e6, 1e6, 434e6, 200)
         self._lsb_freq_win = qtgui.RangeWidget(self._lsb_freq_range, self.set_lsb_freq, "LSB Freq", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._lsb_freq_win)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, 10000, True, 0 if "auto" == "auto" else max( int(float(0.1) * 10000) if "auto" == "time" else int(0.1), 1) )
+        self._volume_range = qtgui.Range(0, 100, 0.100, 0.500, 200)
+        self._volume_win = qtgui.RangeWidget(self._volume_range, self.set_volume, "Volume", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._volume_win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec(0))
+
+        self.uhd_usrp_source_0.set_center_freq(lsb_freq, 0)
+        self.uhd_usrp_source_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_source_0.set_gain(rf_gain, 0)
+        self._tx_gain_range = qtgui.Range(0, 40, 1, 10, 200)
+        self._tx_gain_win = qtgui.RangeWidget(self._tx_gain_range, self.set_tx_gain, "TX Gain", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._tx_gain_win)
         self.blocks_selector_0 = blocks.selector(gr.sizeof_float*1,0,0)
         self.blocks_selector_0.set_enabled(True)
         self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
-        self.blocks_file_sink_1 = blocks.file_sink(gr.sizeof_float*1, '/home/parallels/Desktop/cw-code/data_sharppeak/sharppeak', False)
+        self.blocks_file_sink_1 = blocks.file_sink(gr.sizeof_float*1, '/home/lindnera/data/TestSharpWhisperer/software/cw-code/data_sharppeak/sharppeak', False)
         self.blocks_file_sink_1.set_unbuffered(False)
         self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(1)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.analog_quadrature_demod_cf_0, 0), (self.blocks_selector_0, 0))
         self.connect((self.blocks_selector_0, 1), (self.blocks_file_sink_1, 0))
         self.connect((self.blocks_selector_0, 0), (self.blocks_null_sink_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.analog_quadrature_demod_cf_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.analog_quadrature_demod_cf_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "collect_sharppeak")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "collect_sharppeak")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -129,18 +144,23 @@ class collect_sharppeak(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
     def get_rf_gain(self):
         return self.rf_gain
 
     def set_rf_gain(self, rf_gain):
         self.rf_gain = rf_gain
+        self.uhd_usrp_source_0.set_gain(self.rf_gain, 0)
+        self.uhd_usrp_source_0.set_gain(self.rf_gain, 1)
 
     def get_lsb_freq(self):
         return self.lsb_freq
 
     def set_lsb_freq(self, lsb_freq):
         self.lsb_freq = lsb_freq
+        self.uhd_usrp_source_0.set_center_freq(self.lsb_freq, 0)
+        self.uhd_usrp_source_0.set_center_freq(self.lsb_freq, 1)
 
     def get_freq_shift(self):
         return self.freq_shift
@@ -158,6 +178,7 @@ def main(top_block_cls=collect_sharppeak, options=None):
     tb = top_block_cls()
 
     tb.start()
+    tb.flowgraph_started.set()
 
     tb.show()
 
