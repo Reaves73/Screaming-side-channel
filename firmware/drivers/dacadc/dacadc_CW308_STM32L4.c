03b682aa54693bbafe585465d89bbfc46492e907
Andreas Lindner <andili@kth.se>
2026-05-18 14:16:21 +0200

    add LEDs and soldertest for stm32l4

--- firmware/drivers/dacadc/dacadc_CW308_STM32L4.c
+++ firmware/drivers/dacadc/dacadc_CW308_STM32L4.c
@@ -1,6 +1,9 @@
 #include "stdint.h"
 #include "dacadc.h"
 
+#include "stm32l4xx_hal_rcc.h"
+#include "stm32l4xx_hal_gpio.h"
+
 void dac_set_gate(uint8_t on) {
 }
 
@@ -31,12 +34,87 @@ void adc_init()
 
 uint16_t adc_get()
 {
+    return 0;
 }
 
 uint16_t adc_get_mv()
 {
+    return 0;
 }
 
 void dacadc_init() {
 }
 
+// -----------------------------------
+
+void soldering_pintest_init() {
+	GPIO_InitTypeDef gpio;
+
+	// DAC
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	// DAC channel 1
+	gpio.Pin  = GPIO_PIN_4;
+ 	gpio.Mode = GPIO_MODE_INPUT;
+	gpio.Pull = GPIO_PULLDOWN; //GPIO_NOPULL, GPIO_PULLUP, GPIO_PULLDOWN
+	HAL_GPIO_Init(GPIOA, &gpio);
+
+	// DAC channel 2
+	gpio.Pin  = GPIO_PIN_5;
+ 	gpio.Mode = GPIO_MODE_INPUT;
+	gpio.Pull = GPIO_PULLDOWN; //GPIO_NOPULL, GPIO_PULLUP, GPIO_PULLDOWN
+	HAL_GPIO_Init(GPIOA, &gpio);
+}
+
+uint8_t soldering_pintest_read() {
+    uint8_t v = 0;
+    v |= ((HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) == GPIO_PIN_SET) & 0x1) << 0;
+    v |= ((HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) == GPIO_PIN_SET) & 0x1) << 1;
+    return v;
+}
+
+void soldering_pintest_led_set(void (*_miscgpio_led_set)(uint8_t,uint8_t), uint8_t v) {
+    miscgpio_led_set(1, v & 0x1);
+    miscgpio_led_set(2, v & 0x2);
+}
+
+
+void soldering_pintest_trial(uint8_t v) {
+	GPIO_InitTypeDef gpio;
+	
+	// DAC channel 1
+	gpio.Pin  = GPIO_PIN_4;
+ 	gpio.Mode = GPIO_MODE_INPUT;
+	if (v) {
+		gpio.Pull = GPIO_PULLUP;
+	} else {
+		gpio.Pull = GPIO_PULLDOWN;
+	}
+	HAL_GPIO_Init(GPIOA, &gpio);
+	
+	// DAC channel 2
+	gpio.Pin  = GPIO_PIN_5;
+ 	gpio.Mode = GPIO_MODE_INPUT;
+	if (v) {
+		gpio.Pull = GPIO_PULLDOWN;
+	} else {
+		gpio.Pull = GPIO_PULLUP;
+	}
+	HAL_GPIO_Init(GPIOA, &gpio);
+}
+
+void dac_soldertest(void (*_delay_cycles)(volatile uint32_t), void (*_miscgpio_led_set)(uint8_t,uint8_t)) {
+    soldering_pintest_init();
+	while(1) {
+		_miscgpio_led_set(0, 0);
+		soldering_pintest_trial(0);
+		_delay_cycles(1000000/2);
+        soldering_pintest_led_set(_miscgpio_led_set, soldering_pintest_read());
+		_delay_cycles(1000000/2);
+
+		_miscgpio_led_set(0, 1);
+		soldering_pintest_trial(1);
+		_delay_cycles(1000000/2);
+        soldering_pintest_led_set(_miscgpio_led_set, soldering_pintest_read());
+		_delay_cycles(1000000/2);
+	}
+}
--- firmware/drivers/miscgpio/miscgpio_CW308_STM32L4.c
+++ firmware/drivers/miscgpio/miscgpio_CW308_STM32L4.c
@@ -1,8 +1,45 @@
 #include "stdint.h"
 #include "miscgpio.h"
 
-void miscgpio_init() {
-}
+#include "stm32l4xx_hal_rcc.h"
+#include "stm32l4xx_hal_gpio.h"
 
-void miscgpio_led_set(uint8_t i) {
+void miscgpio_init() {
+	GPIO_InitTypeDef gpio;
+
+	// LED pins
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	gpio.Pin  = GPIO_PIN_15;
+ 	gpio.Mode = GPIO_MODE_OUTPUT_PP;//GPIO_MODE_OUTPUT_PP, GPIO_MODE_OUTPUT_OD;
+	gpio.Pull = GPIO_NOPULL; //GPIO_NOPULL, GPIO_PULLUP, GPIO_PULLDOWN
+	HAL_GPIO_Init(GPIOC, &gpio);
+	gpio.Pin  = GPIO_PIN_14;
+	HAL_GPIO_Init(GPIOC, &gpio);
+	gpio.Pin  = GPIO_PIN_13;
+	HAL_GPIO_Init(GPIOC, &gpio);
+
+	miscgpio_led_set(0, 0);
+	miscgpio_led_set(1, 0);
+	miscgpio_led_set(2, 0);
+}
+void miscgpio_led_set(uint8_t id, uint8_t on) {
+	GPIO_PinState ps;
+	if (on) {
+		ps = SET;
+	} else {
+		ps = RESET;
+	}
+	switch (id) {
+		case 0:
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, ps);
+		    break;
+		case 1:
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_14, ps);
+		    break;
+		case 2:
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, ps);
+		    break;
+		default:
+		    while (1); // wrong id
+	}
 }
