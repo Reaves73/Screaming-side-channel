fc6a534bde084a5296bf83d9b96ce4d90be5427b
Andreas Lindner <andili@kth.se>
2026-05-19 09:46:11 +0200

    add command line interface to capture script

--- software/capture.py
+++ software/capture.py
@@ -6,23 +6,45 @@ import sharpcapturer
 
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
+# parse arguments
+# ---------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument("experiment_name", help="short, concise name for the given experiment (will be part of experiment directory name)")
+
+parser.add_argument("-n", "--n_traces", help="number of traces", type=int, default=1000)
+
+parser.add_argument("--cw_n_samples", help="chipwhisperer samples", type=int, default=12000)
+parser.add_argument("--cw_n_decimate", help="chipwhisperer decimate", type=int, default=1)
+
+args = parser.parse_args()
+
+
+# building the configuration dictionary
+# ---------------------------
 config_dict = {}
-config_dict["experiment_name"] = "testexp"
+config_dict["experiment_name"] = args.experiment_name
 
-config_dict["n_traces"] = 5
+config_dict["n_traces"] = args.n_traces
+#config_dict["n_traces"] = 5
 #config_dict["n_traces"] = 50
 #config_dict["n_traces"] = 5000
 
 config_dict["include_trace_chipwhisperer"] = True
 config_dict["include_trace_gnuradio"] = True
 
-config_dict["chipwhisperer_n_samples"] = 12000
+config_dict["chipwhisperer_n_samples"] = args.cw_n_samples
+#config_dict["chipwhisperer_n_samples"] = 12000
 #config_dict["chipwhisperer_n_samples"] = 24000
 
-config_dict["chipwhisperer_n_decimate"] = 1
+config_dict["chipwhisperer_n_decimate"] = args.cw_n_decimate
+#config_dict["chipwhisperer_n_decimate"] = 1
 #config_dict["chipwhisperer_n_decimate"] = 4
 
+
+# run the capturing function
+# ---------------------------
 sharpcapturer.capture(config_dict)
 
 
