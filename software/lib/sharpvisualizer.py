de9d709731907249ee65f78e27c26c9b3b18f0a1
Andreas Lindner <andili@kth.se>
2026-05-22 17:39:06 +0200

    refactor sharpvisualizer library :)

---
+++ software/lib/sharpvisualizer.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import decimate
+
+def stream_downsample_average(trace, fs: float, duration_s: float, factor: int):
+    total_samples = trace.size
+    #total_samples = file_size // 4  # float32
+    #samples_to_read = min(int(fs * duration_s), total_samples)
+    samples_to_read = total_samples
+    #print(f" {file_size} bytes")
+
+    #print(f" {total_samples}")
+    #print(f" {samples_to_read}")
+    #print(f" {samples_to_read / fs:.6f} s")
+
+    if factor <= 1:
+        raise ValueError("require factor > 1")
+
+    #averaged = []
+
+    # TODO: use fs and duration_s; preserve averaging?
+    trace_ds = decimate(trace, factor)
+    #with open(path, "rb") as f:
+    #    samples_done = 0
+    #    while samples_done < samples_to_read:
+    #        need = min(factor, samples_to_read - samples_done)
+    #        chunk = np.fromfile(f, dtype=np.float32, count=need)
+    #        # TODO: no idea why the first 100 samples of gnuradio are so weird, clipping is a quickfix
+    #        chunk = np.clip(chunk, -4, 4)
+
+    #        if len(chunk) == 0:
+    #            break
+
+    #        averaged.append(np.mean(chunk))
+    #        samples_done += len(chunk)
+
+    #y = np.array(averaged, dtype=np.float32)
+    #fs_down = fs / factor
+    #fs_ds = fs / factor
+    fs_ds = None
+
+    return trace_ds, fs_ds
+
+def plot_time(samples, fs, title="Time Domain"):
+    t = np.arange(len(samples)) / fs
+    plt.figure(figsize=(10, 4))
+    plt.plot(t, samples)
+    plt.title(title)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+def plot_spectrum(samples, fs, title="Spectrum"):
+    n = min(len(samples), 65536)
+    if n < 16:
+        print("sample too less")
+        return
+
+    x = samples[:n] - np.mean(samples[:n])
+    window = np.hanning(n)
+    X = np.fft.rfft(x * window)
+    freqs = np.fft.rfftfreq(n, d=1.0 / fs)
+    mag_db = 20 * np.log10(np.abs(X) + 1e-12)
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(freqs, mag_db)
+    plt.title(title)
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude (dB)")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
--- software/visualize.py
+++ software/visualize.py
@@ -1,82 +1,14 @@
 #!/usr/bin/env python3
 
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.signal import decimate
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/lib")
+
+import sharpvisualizer
+
+import numpy as np
 from pathlib import Path
 import argparse
-
-def stream_downsample_average(trace, fs: float, duration_s: float, factor: int):
-    total_samples = trace.size
-    #total_samples = file_size // 4  # float32
-    #samples_to_read = min(int(fs * duration_s), total_samples)
-    samples_to_read = total_samples
-    #print(f" {file_size} bytes")
-
-    #print(f" {total_samples}")
-    #print(f" {samples_to_read}")
-    #print(f" {samples_to_read / fs:.6f} s")
-
-    if factor <= 1:
-        raise ValueError("require factor > 1")
-
-    #averaged = []
-
-    # TODO: use fs and duration_s; preserve averaging?
-    trace_ds = decimate(trace, factor)
-    #with open(path, "rb") as f:
-    #    samples_done = 0
-    #    while samples_done < samples_to_read:
-    #        need = min(factor, samples_to_read - samples_done)
-    #        chunk = np.fromfile(f, dtype=np.float32, count=need)
-    #        # TODO: no idea why the first 100 samples of gnuradio are so weird, clipping is a quickfix
-    #        chunk = np.clip(chunk, -4, 4)
-
-    #        if len(chunk) == 0:
-    #            break
-
-    #        averaged.append(np.mean(chunk))
-    #        samples_done += len(chunk)
-
-    #y = np.array(averaged, dtype=np.float32)
-    #fs_down = fs / factor
-    #fs_ds = fs / factor
-    fs_ds = None
-
-    return trace_ds, fs_ds
-
-def plot_time(samples, fs, title="Time Domain"):
-    t = np.arange(len(samples)) / fs
-    plt.figure(figsize=(10, 4))
-    plt.plot(t, samples)
-    plt.title(title)
-    plt.xlabel("Time (s)")
-    plt.ylabel("Amplitude")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-def plot_spectrum(samples, fs, title="Spectrum"):
-    n = min(len(samples), 65536)
-    if n < 16:
-        print("sample too less")
-        return
-
-    x = samples[:n] - np.mean(samples[:n])
-    window = np.hanning(n)
-    X = np.fft.rfft(x * window)
-    freqs = np.fft.rfftfreq(n, d=1.0 / fs)
-    mag_db = 20 * np.log10(np.abs(X) + 1e-12)
-
-    plt.figure(figsize=(10, 4))
-    plt.plot(freqs, mag_db)
-    plt.title(title)
-    plt.xlabel("Frequency (Hz)")
-    plt.ylabel("Magnitude (dB)")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
 
 def main():
     # parse arguments
@@ -89,7 +21,7 @@ def main():
     parser.add_argument("--fs", help="sampling frequency", type=float, default=None)
     parser.add_argument("--duration", help="duration in seconds", type=float, default=None)
 
-    parser.add_argument("--averaged_traces", help="how many traces to take and average", type=int)
+    parser.add_argument("--averaged_traces", help="how many traces to take and average", type=int, default=1)
 
     args = parser.parse_args()
 
@@ -119,7 +51,7 @@ def main():
     assert args.fs is None and args.duration is None
     fs = 500000
 
-    y, fs_down = stream_downsample_average(trace, args.fs, args.duration, args.factor)
+    y, fs_down = sharpvisualizer.stream_downsample_average(trace, args.fs, args.duration, args.factor)
     fs_down = fs/args.factor
 
     # 去直流
@@ -128,8 +60,8 @@ def main():
 
     # plot
     # ---------------------------
-    plot_time(y, fs, title=f"Downsampled Time Domain (factor={args.factor})")
-    plot_spectrum(y, fs, title=f"Downsampled Spectrum (factor={args.factor})")
+    sharpvisualizer.plot_time(y, fs, title=f"Downsampled Time Domain (factor={args.factor})")
+    sharpvisualizer.plot_spectrum(y, fs, title=f"Downsampled Spectrum (factor={args.factor})")
 
 
 if __name__ == "__main__":
